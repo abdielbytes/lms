65068c2b9f75dcff55503116f35cbbdc881b6a1c
abdielbytes <wilsonabdiel86@gmail.com>
2024-10-24 15:39:58 +0100

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,20 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<div class="h-screen w-screen">
+    <div class="w-screen">
+        <div>
+            <h3>204 Users</h3>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-black overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+        </div>
+        <div>
+            <h3>30 Academies</h3>
+        </div>
+        <div>
+            <h3>201 Active Users</h3>
+        </div>
+        <div>
+            <h3>3 Inactive users</h3>
+        </div>
+        <div>
+            <h3>0 Disabled users</h3>
         </div>
     </div>
-</x-app-layout>
+</div>
